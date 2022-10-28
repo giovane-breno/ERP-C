a623d4609787f2c38dae3d4774e03a896abbb1a8
Giovane Breno <giovane.breno@gmail.com>
2022-10-28 12:09:47 -0300

    some changes

---
+++ includes/texts.h
@@ -0,0 +1,18 @@
+void return_texts(int type){
+    switch (type)
+    {
+    case 1:
+        printf("|-----------------------------------------------------------|\n");
+        printf("|                        (LOGIN)                            |\n");
+        printf("|-----------------------------------------------------------|\n");
+        printf("|                                                           |\n");
+        printf("|                (1) - Login                                |\n");
+        printf("|                (2) - Cadastrar                            |\n");
+        printf("|                                                           |\n");
+        printf("|-----------------------------------------------------------|\n");
+        break;
+    
+    default:
+        break;
+    }
+}
