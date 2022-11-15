f76d137a9aa770e3ca493ebd76a2115ce8970772
Giovane Breno <giovane.breno@gmail.com>
2022-11-15 18:31:13 -0300

    query workers

--- includes/file_handler.h
+++ includes/file_handler.h
@@ -14,7 +14,6 @@ int file_exist()
         status = 1;
     }
     return status;
-
 }
 
 int config_file()
@@ -25,24 +24,29 @@ int config_file()
         FILE *fp = fopen("files\\config.txt", "a");
         fprintf(fp, "register: 1");
         fclose(fp);
-        status = 1; 
-    }else{
+        status = 1;
+    }
+    else
+    {
         FILE *fp = fopen("files\\config.txt", "r");
         char c;
         while ((c = getc(fp)) != EOF)
         {
             if (c == '1')
             {
-                status = 1; 
-            }else if (c == '0') {
-                status = 0; 
+                status = 1;
+            }
+            else if (c == '0')
+            {
+                status = 0;
             }
         }
     }
     return status;
 }
 
-void check_register_status(){
+void check_register_status()
+{
     FILE *fp = fopen("files\\config.txt", "r");
     char c;
     while ((c = getc(fp)) != EOF)
@@ -54,10 +58,12 @@ void check_register_status(){
             fprintf(fp, "register: 0");
             puts("Desativado!");
             fclose(fp);
-        }else if (c == '0') {
+        }
+        else if (c == '0')
+        {
             fclose(fp);
             FILE *fp = fopen("files\\config.txt", "w+");
-            fprintf(fp, "register: 1");   
+            fprintf(fp, "register: 1");
             puts("Ativado!");
             fclose(fp);
         }
@@ -130,14 +136,44 @@ void query_c_files(char filename[25])
 
     while (fscanf(fp, "%s %d %s %d", name, &age, cpf, &active) == 4)
     {
-        {
-            printf("Consulta: %d\n\n", counter);
-            printf("Nome: %s\n", name);
-            printf("Idade: %d anos\n", age);
-            printf("CPF: %s\n", cpf);
-            (active == 1) ? printf("Ativo? Sim\n") : printf("Ativo? Nao\n");
-            puts("-----------------------------------------------------------");
-        }
+
+        printf("Consulta: %d\n\n", counter);
+        printf("Nome: %s\n", name);
+        printf("Idade: %d anos\n", age);
+        printf("CPF: %s\n", cpf);
+        (active == 1) ? printf("Ativo? Sim\n") : printf("Ativo? Nao\n");
+        puts("-----------------------------------------------------------");
+
+        counter++;
+    }
+    fclose(fp);
+}
+
+void query_w_files(char filename[25])
+{
+    char name[256];
+    char role[256];
+    float payment;
+    int counter = 1;
+
+    FILE *fp = fopen(filename, "r");
+    char c;
+    if (!fp)
+    {
+        printf("Erro na abertura do arquivo!");
+    }
+
+    bool password = false;
+
+    while (fscanf(fp, "%s %s %f", name, role, &payment) == 3)
+    {
+
+        printf("Consulta: %d\n\n", counter);
+        printf("Nome: %s\n", name);
+        printf("Cargo: %s\n", role);
+        printf("Salario: %0.2f\n", payment);
+        puts("-----------------------------------------------------------");
+
         counter++;
     }
     fclose(fp);
